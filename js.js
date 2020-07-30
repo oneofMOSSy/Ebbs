596404f447eb2659129d26a980af514ac36fa4d0
oneofMOSSy <oneofMOSSy@users.noreply.github.com>
2020-07-30 23:11:17 +0200

    Initial commit

---
+++ js.js
@@ -0,0 +1,3 @@
+$('#text1').click(function(){
+    $('#demo').text("Du är världens finaste! Du är den som gör mig glad. när jag stiger upp på morgonen så tänker jag vilken tur jag har som får vara med dig och tillbringa tid med dig <3");
+});
